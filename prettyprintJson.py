58080f33b27d2a7437e5d346c055c9fad9f92f3d
OMEEDILYAS <ilyasomeed7@gmail.com>
2025-12-15 22:52:03 -0600

    cleaning up the repo

--- prettyprintJson.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# Load the JSON data from a file
-with open('wi_2024_gen_prec_graph.json', 'r') as f:
-    data = json.load(f)
-
-with open('pretty_print.json', 'w') as f:
-    json.dump(data, f, indent=4)
